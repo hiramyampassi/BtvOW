cef39cb4ce7edd9c77aab708ede0dd422d189b85
hiramyampassi <hiramyamen@gmail.com>
2017-10-12 10:44:34 -0400

    dev1

--- admin/Views/main.php
+++ admin/Views/main.php
@@ -19,35 +19,50 @@ class Template {
 
         </head>
         <body>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-              <a class="navbar-brand" href="#">Bolivia TV</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+          <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Bolivia TV</a>
+              </div>
+
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Usuarios</a></li>
+                      <li><a href="#">Secciones</a></li>
+                      <li><a href="#">Regiones</a></li>
+                    </ul>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URL; ?>noticias">Noticias</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Albumes</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Admin
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <a class="dropdown-item" href="#">Usuarios</a>
-                      <a class="dropdown-item" href="#">Secciones</a>
-                      <a class="dropdown-item" href="#">Regiones</a>
-                    </div>
+                  <li><a href="<?php echo URL; ?>noticias">Noticias</a></li>
+                  <li><a href="<?php echo URL; ?>noticias">Docs web</a></li>
+                  <li><a href="<?php echo URL; ?>noticias">Banner sup</a></li>
+                  <li><a href="<?php echo URL; ?>noticias">Eventos especiales</a></li>
+                  <li><a href="<?php echo URL; ?>noticias">Youtube</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Nombre de usuario <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Datos personales</a></li>
+                      <li><a href="#">Ayuda</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">Cerrar sesi&oacute;n</a></li>
+                    </ul>
                   </li>
                 </ul>
-              </div>
-            </nav>
+              </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+          </nav>
 
 
 <?php
